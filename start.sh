19bed48d378fe04a32d8d85708dad4aa3f38f8de
cometiklab-beep <cometi.k.lab@gmail.com>
2025-09-29 17:31:06 -0500

    Delete start.sh

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Ejecuta el comando de inicio robusto
-# Esto es más seguro que solo el Procfile en algunos entornos.
-/opt/render/project/src/.venv/bin/python -m gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:$PORT
